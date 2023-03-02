6b0396a3b6933f9dc971301c9bbd99ce963739cf
laszlogulyas-seon <laszlo.gulyas@seon.io>
2023-03-02 18:04:16 +0100

    add dockerized spring rest app hello-world

---
+++ spring-app-hello-world/src/main/java/com/laca/springapphelloworld/SpringAppHelloWorldApplication.java
@@ -0,0 +1,14 @@
+package com.laca.springapphelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringAppHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppHelloWorldApplication.class, args);
+    }
+
+}
---
+++ spring-app-hello-world/src/test/java/com/laca/springapphelloworld/SpringAppHelloWorldApplicationTests.java
@@ -0,0 +1,14 @@
+package com.laca.springapphelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SpringAppHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
