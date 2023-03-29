3d8034fa8a29d3874fb0005f6e6538fee78f0d33
laszlogulyas-seon <laszlo.gulyas@seon.io>
2023-03-29 16:20:00 +0200

    add configmaps

--- spring-app-hello-world/run-docker-container.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-docker run -d -p 8080:8080 --name spring-app-hello-world spring-app-hello-world
--- spring-app-hello-world/src/main/java/com/laca/springapphelloworld/controller/CoreController.java
+++ spring-app-hello-world/src/main/java/com/laca/springapphelloworld/controller/CoreController.java
@@ -1,5 +1,6 @@
 package com.laca.springapphelloworld.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/core")
 public class CoreController {
 
+    @Value("${PRINT_TEXT:default}")
+    private String textToPrint;
+
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello World!");
+        StringBuilder responseBody = new StringBuilder()
+          .append("Hello World!").append("\n")
+          .append("Value of PRINT_TEXT: ").append(textToPrint);
+        return ResponseEntity.ok(responseBody.toString());
     }
 
 }
